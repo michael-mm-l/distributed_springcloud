140fef1847dc8620cbf3996c53f03f3dcd0cfa49
michael <15689670104@163.com>
2020-11-03 21:24:42 +0800

    xiugai

--- register_center/src/main/java/com/distributed_springcloud/register_center/RegisterCenterApplication.java
+++ register_center/src/main/java/com/distributed_springcloud/register_center/RegisterCenterApplication.java
@@ -2,7 +2,9 @@ package com.distributed_springcloud.register_center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class RegisterCenterApplication {
 
