7e1034420bd57d9fb3842e33f3299c6721fc92c8
michael <15689670104@163.com>
2020-11-03 20:43:53 +0800

    xiugai

---
+++ register_center/src/main/java/com/distributed_springcloud/register_center/RegisterCenterApplication.java
@@ -0,0 +1,13 @@
+package com.distributed_springcloud.register_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterCenterApplication.class, args);
+	}
+
+}
---
+++ register_center/src/test/java/com/distributed_springcloud/register_center/RegisterCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributed_springcloud.register_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
