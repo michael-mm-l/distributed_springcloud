4868943fc5ea9909a09f3ca347bb66cea0dae386
michael <15689670104@163.com>
2020-11-06 21:28:28 +0800

    add actuator

---
+++ server_three/src/main/java/org/distributed_springcloud/server_three/api/ServiceController.java
@@ -0,0 +1,20 @@
+package org.distributed_springcloud.server_three.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ServiceController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+
+    @GetMapping("/api")
+    public void server(){
+        log.info("{} is api",applicationName);
+    }
+}
